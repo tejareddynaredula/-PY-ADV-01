6ad866fca75a1bc377a097677baef3ca55ef9025
Tejareddy Naredula <93875958+tejareddynaredula@users.noreply.github.com>
2026-08-31 15:28:45 +0530

    Completed PY-ADV-06 Task 14 - Type Hints

--- PY-ADV-06/app/utils/helpers.py
+++ PY-ADV-06/app/utils/helpers.py
@@ -1,4 +1,9 @@
-def find_student_by_id(students: list, student_id: int):
+from app.models.student import Student
+
+
+def find_student_by_id(
+    students: list[Student], student_id: int
+) -> Student | None:
     """Find a student by ID."""
     for student in students:
         if student.student_id == student_id:
